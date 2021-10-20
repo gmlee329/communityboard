ef2c0e99156f718766ebcb4529684429be5ce4f1
gyumin.lee <gmlee329@naver.com>
2021-10-20 10:54:15 +0900

    test : db connection test 추가
    mariadb connection test

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.backend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Slf4j
 @SpringBootTest
 class BackendApplicationTests {
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void dbConnectionTest() throws SQLException {
+        try(Connection connection = dataSource.getConnection()) {
+            log.info("dbcp : " + connection.getClass());
+            log.info("url : " + connection.getMetaData().getURL());
+            log.info("username : " + connection.getMetaData().getUserName());
+
+        }
     }
 
 }
